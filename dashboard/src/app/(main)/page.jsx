5872cc3abddea3a68d55d392eaf159aa5dfd3aa0
Admin <solankikishan149@gmail.com>
2025-10-13 17:40:50 +0530

    upsated

--- dashboard/src/app/(main)/page.jsx
+++ dashboard/src/app/(main)/page.jsx
@@ -16,7 +16,7 @@ export default async function Home() {
   }
 
   return (
-    <main className="flex flex-col flex-auto items-center justify-between p-2">
+    <main className="flex flex-col flex-auto items-center justify-between p-27">
       <h1 className="text-4xl font-bold">Welcome to W! Platform!</h1>
       <Image
         src="/logo.svg"
